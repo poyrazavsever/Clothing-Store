14e5b3516b7c270f82c38dfd1cca735008c924cf
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-07-30 11:17:19 +0300

    feat: complated file system

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '1024px',
+    },
+    extend: {
+      animation: {
+        sliderAnimation: 'scroll 40s linear infinite',
+        sliderStart: 'sliderStart .6s ease-in-out',
+      },
+
+
+      keyframes: {
+        scroll: {
+          '0%' : {transform: 'translateX(0)' },
+          '100%': {transform : 'translateX(-3500px)'}
+        },
+        sliderStart: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '100' },
+        }
+      }
+    },
+  },
+  plugins: [],
+};
