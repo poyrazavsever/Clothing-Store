2b5886331db83672021a893c28d196a3ed1bdb62
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-01 08:09:26 +0300

    feat:login modal done

--- Components/Auth/LoginModal.jsx
+++ Components/Auth/LoginModal.jsx
@@ -7,7 +7,7 @@ function LoginModal({ isLoginVisible, setIsLoginVisible }) {
 
     const [visiblePassword, setVisiblePassword] = useState(false)
     const passwordBtn = "text-2xl absolute top-[9px] right-3"
-    const lineStyle = "h-[2px] w-full rounded bg-tert-200"
+    const lineStyle = "h-[1px] w-full rounded bg-neutral-200"
 
     const handlePassword = (e) => {
         e.preventDefault()
@@ -21,20 +21,9 @@ function LoginModal({ isLoginVisible, setIsLoginVisible }) {
             "hidden": !isLoginVisible
         })}>
 
-            <div className='h-[600px] w-[400px] bg-neutral-950 bg-opacity-50 border border-neutral-950 p-8 border-tert-200 rounded-sm'>
+            <div className='h-[580px] w-[400px] bg-neutral-950 bg-opacity-50 border border-neutral-950 p-8 border-tert-200 rounded-sm'>
 
-                <div className="flex w-full items-start justify-between">
-
-                    <h2 className='text-2xl font-semibold tracking-widest text-zinc-300'>Log In</h2>
-
-                    <button onClick={() => setIsLoginVisible(false)}>
-                        <Icon iconType="ai" iconName="AiOutlineCloseCircle" iconColor="text-zinc-300" classname="text-2xl pr-8" />
-                    </button>
-
-
-                </div>
-
-                <form className="flex flex-col items-start gap-4 w-full">
+                <form className="flex flex-col items-start gap-10 w-full">
 
                     <div className="w-full flex flex-col gap-3">
                         <label htmlFor="email" className="text-white text-xs font-semibold uppercase tracking-widest">E-mail</label>
@@ -42,8 +31,8 @@ function LoginModal({ isLoginVisible, setIsLoginVisible }) {
                         <input id="email" type="email" placeholder='example@outlook.com' className='w-full px-4 py-2 bg-neutral-950 opacity-80 focus:outline-none border-neutral-950 placeholder:text-sm placeholder:tracking-wide placeholder:text-zinc-500 text-white' />
                     </div>
 
-                    <div className="w-full">
-                        <label htmlFor="email" className="text-white text-xs font-semibold uppercase tracking-widest">E-mail</label>
+                    <div className="w-full flex flex-col gap-3">
+                        <label htmlFor="email" className="text-white text-xs font-semibold uppercase tracking-widest">Password</label>
 
                         <div className="w-full relative">
                             <input type={visiblePassword ? "text" : "password"} placeholder='********' className='w-full px-4 py-2 bg-neutral-950 opacity-80 focus:outline-none border-neutral-950 placeholder:text-sm placeholder:tracking-wide placeholder:text-zinc-500 text-white' />
@@ -59,35 +48,35 @@ function LoginModal({ isLoginVisible, setIsLoginVisible }) {
 
 
 
-                    <button className="flex items-center justify-center w-full px-4 py-2 rounded-sm bg-tert-200 text-white hover:bg-tert-100 transition-all">Giriş Yap</button>
+                    <button className="flex uppercase tracking-widest text-sm font-semibold items-center justify-center w-full px-4 py-3 rounded-sm bg-amber-600 text-white hover:bg-orange-700 transition-all">Log In</button>
 
                 </form>
 
                 <div className="pt-5 flex flex-col items-start gap-3">
                     <div className="flex flex-col items-start gap-1">
-                        <p className="text-xs text-neutral-500">Şifrenizi mi unuttunuz? <Link href="/" className="font-medium  text-neutral-700">Buradan yenileyin.</Link></p>
+                        <p className="text-xs text-neutral-400">Forgot your password?  <Link href="/" className="font-medium  text-neutral-200">Reset it here</Link></p>
                     </div>
                     <div className="flex flex-col items-start gap-1">
-                        <p className="text-xs text-neutral-500">Daha önce kayıt olmadınız mı? <Link href="/" className="font-medium  text-neutral-700">Buradan kayıt olun.</Link></p>
+                        <p className="text-xs text-neutral-400">Not registered yet?  <Link href="/" className="font-medium  text-neutral-200">Register here.</Link></p>
                     </div>
                 </div>
 
-                <div className="flex items-center gap-8 mt-8">
+                <div className="flex items-center justify-center gap-8 mt-8">
                     <div className={lineStyle}></div>
-                    <h4 className="uppercase text-lg font-medium text-tert-200 w-48">ya da</h4>
+                    <h4 className="uppercase text-lg font-medium text-neutral-200 w-48 text-center">OR</h4>
                     <div className={lineStyle}></div>
                 </div>
 
                 <div className="flex flex-col items-start gap-4 mt-5">
 
-                    <button className="flex items-center text-sm gap-2 justify-center w-full px-4 py-2 rounded-sm bg-primary text-neutral-800 tracking-wider">
+                    <button className="flex font-medium hover:bg-neutral-700 items-center text-sm gap-2 justify-center w-full px-4 py-3 rounded-sm bg-neutral-800 text-neutral-200 tracking-wider transition-all ">
                         <Icon iconType="fc" iconName="FcGoogle" classname="text-xl" iconColor="text-neutral-600" />
-                        <span>Google ile Giriş Yap</span>
+                        <span>Log in with google</span>
                     </button>
 
-                    <button className="flex items-center text-sm gap-2 justify-center w-full px-4 py-2 rounded-sm bg-primary text-neutral-800 tracking-wider">
-                        <Icon iconType="bi" iconName="BiLogoFacebookCircle" classname="text-xl text-blue-600" iconColor="text-neutral-600" />
-                        <span>Facebook ile Giriş Yap</span>
+                    <button className="flex font-medium hover:bg-neutral-700 items-center text-sm gap-2 justify-center w-full px-4 py-3 rounded-sm bg-neutral-800 text-neutral-200 tracking-wider transition-all ">
+                        <Icon iconType="bi" iconName="BiLogoFacebookCircle" classname="text-xl " iconColor="text-blue-400" />
+                        <span>Login with facebook</span>
                     </button>
 
                 </div>
