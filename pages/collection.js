25d077de75b24e105bb5711712f80f4d383314ad
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-02 22:03:46 +0300

    feat: collection product sec created

--- pages/collection.js
+++ pages/collection.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 //Pages
 import Banner from '@/Components/Collection/Banner'
+import Products from '@/Components/Collection/Products'
 
 function collection() {
   return (
     <div>
         <Banner />
+        <Products />
     </div>
   )
 }
