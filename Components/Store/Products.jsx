d797f56c9025d11cf63569e7f909c943c65fb5b5
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-03 13:43:21 +0300

    fix: grid and flex replaced

--- Components/Store/Products.jsx
+++ Components/Store/Products.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ProductCard from '../ProductCard'
 function Products() {
   return (
-    <div className='dark grid lg:grid-cols-2 xl:grid-cols-4 gap-8'>
+    <div className='dark flex flex-wrap gap-8'>
         <ProductCard />
         <ProductCard />
         <ProductCard />
