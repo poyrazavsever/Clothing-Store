469fde37c0432b8bc41d713f5582330c6ae3a47e
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-03 11:49:18 +0300

    feat: created blog page

---
+++ Components/Blog/TopMenu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function TopMenu() {
+  return (
+    <div>
+
+      <div>
+
+        <h1>Luxe Threads</h1>
+
+      </div>
+
+      <div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default TopMenu
---
+++ pages/blog.js
@@ -0,0 +1,16 @@
+import TopMenu from '@/Components/Blog/TopMenu'
+import React from 'react'
+
+function blog() {
+  return (
+    <div className='mt-36'>
+
+        <div>
+            <TopMenu />
+        </div>
+
+    </div>
+  )
+}
+
+export default blog
