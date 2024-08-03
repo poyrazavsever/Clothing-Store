cd4c7526410740cdf63c8396ceb76af0bf2084a7
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-03 12:21:57 +0300

    feat: add card item

---
+++ Components/Blog/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Parallax } from 'react-scroll-parallax'
+
+import CardItem from '../CardItem'
+
+function Post() {
+  return (
+    <div className='h-screen'>
+        <CardItem img_link="/Blog/post.png" pp_foto="/Blog/pp.png" name="Poyraz" title="#SummerInVogue: Aslı İlkbahar ile Bir Yaz Rüyası" descr="Blinq markasının kurucularından Aslı İlkbahar, sımsıcak mevsime dair tüm tercihlerini Vogue Türkiye için listeledi." time="11/06/2019" views="3994" like="400"/>
+    </div>
+  )
+}
+
+export default Post
--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,21 @@
-import TopMenu from '@/Components/Blog/TopMenu'
 import React from 'react'
+
+//Components 
+import Post from '@/Components/Blog/Post'
+import TopMenu from '@/Components/Blog/TopMenu'
 
 function blog() {
   return (
-    <div className='mt-36'>
+    <div className='mt-36 mb-12'>
 
         <div>
             <TopMenu />
         </div>
 
+
+        <div>
+            <Post />
+        </div>
     </div>
   )
 }
