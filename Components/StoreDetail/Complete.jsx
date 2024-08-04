ab22fd7474334365cae328dd31f38d628202a5d6
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-04 14:00:17 +0300

    feat: added flex wrap

--- Components/StoreDetail/Complete.jsx
+++ Components/StoreDetail/Complete.jsx
@@ -6,7 +6,7 @@ function Complete() {
     return (
         <div className='relative h-full !overflow-hidden flex flex-col items-start justify-center gap-16 py-32 dark'>
 
-            <div className='container mx-auto flex flex-col items-start gap-12'>
+            <div className='container mx-auto flex flex-col items-start justify-center gap-12'>
 
                 <div>
                     <h2 className='text-lg font-medium tracking-widest text-zinc-200'><span className='text-transparent font-bold bg-clip-text bg-gradient-to-r from-zinc-300 to-orange-400'>Complete</span> the look</h2>
@@ -14,7 +14,11 @@ function Complete() {
                 </div>
 
 
-                <Parallax speed={-5} className='flex items-cente justify-center gap-12 pt-4'>
+                <Parallax speed={-5} className='flex flex-wrap items-center justify-start gap-12 pt-4'>
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
                     <ProductCard />
                     <ProductCard />
                     <ProductCard />
@@ -28,6 +32,7 @@ function Complete() {
             {/* BG */}
 
             <div className='w-36 h-36 rounded-full bg-gradient-to-r from-zinc-900 to-orange-500 top-32 left-32 absolute -z-50 opacity-60 blur-3xl'></div>
+            <div className='w-1/2 h-full bg-neutral-900 opacity-75 absolute right-0 bottom-0 -z-20'></div>
         </div>
     )
 }
