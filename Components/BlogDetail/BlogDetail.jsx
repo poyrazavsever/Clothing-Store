af577e7fd6291cb722f9072ed2c321916a563e9a
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-04 15:10:05 +0300

    feat: done detail page

--- Components/BlogDetail/BlogDetail.jsx
+++ Components/BlogDetail/BlogDetail.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import Write from './Write'
 
 function BlogDetail() {
   return (
     <div>
         <Banner />
+        <Write />
     </div>
   )
 }
