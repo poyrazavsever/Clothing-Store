50c79e840eb462152241f6189ea82b54d382c1f1
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-04 12:03:42 +0300

    feat: done complete

---
+++ Components/StoreDetail/Complete.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Parallax } from 'react-scroll-parallax'
+import ProductCard from '../ProductCard'
+import ReactIcon from '../Icon'
+function Complete() {
+    return (
+        <div className='relative h-full !overflow-hidden flex flex-col items-start justify-center gap-16 py-32 dark'>
+
+            <div className='container mx-auto flex flex-col items-start gap-12'>
+
+                <div>
+                    <h2 className='text-lg font-medium tracking-widest text-zinc-200'><span className='text-transparent font-bold bg-clip-text bg-gradient-to-r from-zinc-300 to-orange-400'>Complete</span> the look</h2>
+                    <span className='text-xs font-medium text-zinc-400'>The best products for you.</span>
+                </div>
+
+
+                <Parallax speed={-5} className='flex items-cente justify-center gap-12 pt-4'>
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </Parallax>
+
+
+            </div>
+
+
+            {/* BG */}
+
+            <div className='w-36 h-36 rounded-full bg-gradient-to-r from-zinc-900 to-orange-500 top-32 left-32 absolute -z-50 opacity-60 blur-3xl'></div>
+        </div>
+    )
+}
+
+export default Complete
--- Components/StoreDetail/Detail.jsx
+++ Components/StoreDetail/Detail.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ProductDetail from './ProductDetail'
+import Complete from './Complete'
 
 function Detail() {
   return (
     <div className='mt-16'>
       <div className='py-12'>
         <ProductDetail />
+      </div>
+      <div>
+        <Complete />
       </div>
 
     </div>
